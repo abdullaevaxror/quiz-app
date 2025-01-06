302d1e9a5ec90d43b9c67a28ea932941e1cff48d
Mexriddin <mekkinuriddinov@gmail.com>
2025-01-06 19:54:31 +0500

    views page added

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
 use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\Web\HomeController;
 use src\Router;
 
 
 Router::get('/todos', [UserController::class, 'index']);
+Router::get('/home', [HomeController::class, 'home']);
+Router::get('/about', [HomeController::class, 'about']);
+Router::get('/login', [HomeController::class, 'login']);
+Router::get('/register', [HomeController::class, 'register']);
 
-Router::get('/', function (){
-    echo 'Hello World';
-});
+
+
