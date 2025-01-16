e6fdf97e77949c8ad367eb351f116271e81f804c
Mexriddin <mekkinuriddinov@gmail.com>
2025-01-16 14:56:16 +0500

    question added database

--- app/Http/Controllers/API/QuizController.php
+++ app/Http/Controllers/API/QuizController.php
@@ -2,20 +2,28 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Models\Question;
+use App\Models\Quiz;
+use App\Traits\Validator;
 use Src\Auth;
+
+
 
 class QuizController
 {
+    use Validator;
 
     public function store()
     {
-        if (Auth::check()) {
-            $headers = getallheaders();
-            $bearer = $headers['Authorization'];
-            $token = str_replace('Bearer ', '', $bearer);
-            apiResponse([
-                'message' => 'Quiz create successfully',
-            ],201);
-        }
+        $quizItems=$this->validate([
+            'title'=>'string',
+            'description'=>'string',
+            'timeLimit'=>'int',
+            'questions'=>'array',
+        ]);
+        $quiz=new Quiz();
+        $question=new Question();
+        $quiz_id=$quiz->create(Auth::user()->id,$quizItems['title'],$quizItems['description'],$quizItems['timeLimit']);
+        apiResponse(['message'=>'created succesfully'], 201);
     }
 }
--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,7 +2,18 @@
 
 namespace App\Models;
 
-class Question
+class Question extends DB
 {
+    public function create(int $quizId, string $questionText)
+    {
+        $query = "INSERT INTO quiz_question (quiz_id, question_text, updated_at, created_at)
+                    VALUES (:quizId, :questionText, NOW(), NOW())";
+                $stmt = $this->conn->prepare($query);
+                $stmt->execute([
+                    'quizId' => $quizId,
+                    'questionText' => $questionText,
+                ]);
+                return $this->conn->lastInsertId();
+    }
 
 }
